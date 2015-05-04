a50deee4cd099a52e098da78f342346487a3b643
Kim, Jae Chan <kjc0210@gmail.com>
2015-05-04 20:26:29 +0900

    make test for multiset add/remove

--- test/multiset_test.rb
+++ test/multiset_test.rb
@@ -9,4 +9,14 @@ class MultisetTest < Minitest::Test
   def test_contains
     assert_equal true, @set.contains(1)
   end
+
+  def test_add
+    @set.add(2)
+    assert_equal true, @set.contains(2)
+  end
+
+  def test_remove
+    @set.remove(1)
+    assert_equal false, @set.contains(1)
+  end
 end
