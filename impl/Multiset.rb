15cb0a636551b381b27db0e1a6877c2409eaf89c
Kim, Jae Chan <kjc0210@gmail.com>
2015-05-04 20:22:11 +0900

    implement multiset initialize/contains

--- impl/Multiset.rb
+++ impl/Multiset.rb
@@ -1,1 +1,15 @@
+class Multiset
+  def initialize(*args)
+    @set = args
+  end
 
+  def contains(n)
+    result = false
+
+    @set.each do |i|
+      result = true if i == n
+    end
+
+    return result
+  end
+end
